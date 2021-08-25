ddf3fd5a07e9486c23cce65511deae51caee26c7
t203940 <t203940@HP640G4-23944>
2021-08-25 08:44:04 -0300

    Add project files.

---
+++ API/Models/Connector.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace API.Models
+{
+    public class Connector
+    {
+        public int Id { get; set; }
+        public decimal MaxCurrent { get; set; }
+        public Guid ChargeStationId { get; set; }
+    }
+}
---
+++ API/Models/Group.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API.Models
+{
+    public class Group
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Capacity { get; set; }
+        public ICollection<ChargeStation> ChargeStations { get; set; }
+    }
+}

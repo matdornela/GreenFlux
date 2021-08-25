44b3fe5bd809631ad1ec381a807a4cb25042305e
Mateus Dornela <mateusmascarenhas3@gmail.com>
2021-08-25 09:12:26 -0300

    Adding migration and DB context

---
+++ API/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GreenFlux.API.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ChargeStation> ChargeStations { get; set; }
+        public DbSet<Connector> Connectors { get; set; }
+        public DbSet<Group> Groups { get; set; }
+    }
+}
